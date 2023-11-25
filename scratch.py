9efd445d81078caf500e1f76d8e90287abbb06da
Hakerkoder <129597991+Blackraven4252@users.noreply.github.com>
2023-11-25 16:30:51 +0300

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,57 @@
+#1
+print("№1---")
+print(10, end = " ")
+print(5, end = " ")
+print(24)
+print()
+
+#2
+print("№2----")
+print("103,", end = " ")
+print("25,", end = " ")
+print(724)
+print()
+#3
+print("№3----")
+print(1)
+print(2)
+print()
+print(3)
+print()
+#4
+print("№4----")
+a = 3.1415926
+b =  3.142
+print(a)
+print(b)
+print()
+#5
+
+print("№5----")
+q1 = 8 -3
+q2 = 8 - 1
+q3 = 8 -2
+print(" " * q1 + "123", end = " " * q3)
+print(13)
+print(" " * q3 + "56", end  = " " * q1)
+print(355)
+print(" " * q1 + "587", end = " "* q2)
+print(8)
+print()
+#6
+print("№6----")
+q1 = 8 - 3
+q2 = 8  - 4
+q3 = 5
+print(" " * q1 + "1.24", end = " " * q2)
+print(13.52)
+print(" " * 4 + "3.567", end = " "*3)
+print(-355.1)
+print(" " * 6 + "8.2", end = " "*5)
+print(9.18)
+print()
+#7
+print("№7----")
+print(" " * 5 + "7.240" + " " *3 + "-43.520")
+print(" " * 4 +"23.500" + " " * 4 + "55.107")
+print(" " * 4 +"88.203" + " "*2 + "-769.800")
